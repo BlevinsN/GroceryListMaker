3ceb6ec5bb528d0a8ae8314d6d81079143fc1736
Blevins <nathanc.blevins@gmail.com>
2025-07-30 16:11:46 -0500

    Working on the front end. Connecting the postgres db to the front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,24 @@
-import React from 'react'
-import { Table } from "@chakra-ui/react";
-import { MdDelete } from "react-icons/md";
-import { FaEdit } from "react-icons/fa";
+import React from 'react';
+import { VStack } from '@chakra-ui/react';
+import RecipeTable from "./components/ui/RecipeTable";
+import { useQuery } from "@tanstack/react-query";
+import {baseUrl} from "../constants/global-variable.js";
 
 const App = () => {
-  return (
-    <Table.ScrollArea borderWidth="1px" rounded="md" height="160px">
-      <Table.Root size="md" stickyHeader>
-        <Table.Header>
-          <Table.Row bg="bg.subtle">
-            <Table.ColumnHeader>Id</Table.ColumnHeader>
-            <Table.ColumnHeader>Name</Table.ColumnHeader>
-            <Table.ColumnHeader>Dish Type</Table.ColumnHeader>
-            <Table.ColumnHeader>Dish Creator</Table.ColumnHeader>
-            <Table.ColumnHeader>Servings</Table.ColumnHeader>
-            <Table.ColumnHeader>Actions</Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
 
-        <Table.Body>
-          {items.map((item) => (
-            <Table.Row key={item.id}>
-              <Table.Cell>{item.id}</Table.Cell>
-              <Table.Cell>{item.dish_type}</Table.Cell>
-              <Table.Cell>{item.dish_name}</Table.Cell>
-              <Table.Cell>{item.dish_creator}</Table.Cell>
-              <Table.Cell>{item.servings}</Table.Cell>
-              <Table.Cell>
-                <MdDelete size={20}/>
-                <FaEdit size={20}/>
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-    </Table.ScrollArea>
+  async function fetchRecipeDetails(params) {
+    const res = await fetch(baseUrl);
+  }
+
+  const {isPending,isError,data,error} = useQuery({
+    queryKey: ["recipe_details"],
+    queryFn: fetchRecipeDetails
+  })
+  return (
+    <VStack gap="6" align="flex-start">
+      <RecipeTable/>
+    </VStack>
   )
 };
-
-const items = [
-  { id: 1, dish_name: "Corned Beef", dish_type:"dinner", dish_creator: "Nate", servings: 4 },
-  { id: 2, dish_name: "Corned Beef", dish_type:"dinner", dish_creator: "Nate", servings: 4 },
-  { id: 3, dish_name: "Corned Beef", dish_type:"dinner", dish_creator: "Nate", servings: 4 },
-  { id: 4, dish_name: "Corned Beef", dish_type:"dinner", dish_creator: "Nate", servings: 4 },
-  { id: 5, dish_name: "Corned Beef", dish_type:"dinner", dish_creator: "Nate", servings: 4 },
-  { id: 6, dish_name: "Corned Beef", dish_type:"dinner", dish_creator: "Nate", servings: 4 },
-]
 
 export default App
